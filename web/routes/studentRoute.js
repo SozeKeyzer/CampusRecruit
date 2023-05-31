909736cb3c23e7971059fae74ee058013728e814
Ujjwal Raturi <ujjwalraturi248001@gmail.com>
2023-05-31 22:44:56 +0530

    student dashboard and profile

--- web/routes/studentRoute.js
+++ web/routes/studentRoute.js
@@ -4,5 +4,8 @@ const studentController=require('../controller/studentController');
 
 router.get('/studentRegister',studentController.getStudentRegister);
 router.get('/studentLogin',studentController.getStudentLogin);
+router.get('/studentDashboard',studentController.getStudentDashboard);
+router.get('/studentJobPosting',studentController.getStudentJobPosting);
+router.get('/studentProfile',studentController.getStudentProfile);
 
 module.exports=router;
